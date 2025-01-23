3a40c824234971cf7631710d2ddb9f4a72f51514
MD ABDULLAH <MD ABDULLAH>
2025-01-23 22:24:29 +0600

    solve cors prblem

--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,14 @@ const app: Application = express();
 // Use cookie-parser middleware
 app.use(cookieParser());
 
-app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'https://sparkwave-web.vercel.app'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    optionsSuccessStatus: 200,
+  })
+);
 
 // application routes
 app.use('/api', router);
